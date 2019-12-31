70e82c6b16e332b25aee82fe51f531ce9417f73d
treark21 <treark21@gmail.com>
2019-12-30 19:16:41 -0600

    creating sign in

--- app/app.js
+++ app/app.js
@@ -22,13 +22,47 @@ angular
       })
       .state('login', {
         url: '/login',
-        templateUrl: 'auth/login.html'
+        controller: 'AuthCtrl as authCtrl',
+        templateUrl: 'auth/login.html',
+        resolve: {
+          requireNoAuth: function($state, Auth){
+            return Auth.$requireSignIn().then(function(auth){
+              $state.go('home');
+            }, function(error){
+              return;
+            });
+          }
+        }
       })
       .state('register', {
         url: '/register',
-        templateUrl: 'auth/register.html'
+        controller: 'AuthCtrl as authCtrl',
+        templateUrl: 'auth/register.html',
+        resolve: {
+          requireNoAuth: function($state, Auth){
+            return Auth.$requireSignIn().then(function(auth){
+              $state.go('home');
+            }, function(error){
+              return;
+            });
+          }
+        }
       });
 
     $urlRouterProvider.otherwise('/');
   })
-  .constant('FirebaseUrl', 'https://slack.firebaseio.com/');
+  .config(function(){
+    var firebaseConfig = {
+      apiKey: "AIzaSyC3NSBqNSXW_9DwjLCw9uEmibc5jateCaQ",
+      authDomain: "cap-proj-practice.firebaseapp.com",
+      databaseURL: "https://cap-proj-practice.firebaseio.com",
+      projectId: "cap-proj-practice",
+      storageBucket: "cap-proj-practice.appspot.com",
+      messagingSenderId: "1005077049627",
+      appId: "1:1005077049627:web:6f34e24009a23e1ce8a3be",
+      measurementId: "G-4C2X5XM339"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+  
+  });
